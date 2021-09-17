ebec4fdf6909fe33c3ef8990bb77a2746d491291
Pratik <pratik.k048@gmail.com>
2021-09-17 16:20:17 +0530

    make it better2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,9 +3,21 @@ module.exports = {
     // USER: "sql6434954",
     // PASSWORD: "J7be9s3y9P",
     // DB: "sql6434954"
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "server",
-    DB: "tournament"
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "server",
+    // DB: "tournament"
+
+
+
+
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b346791faac424",
+    PASSWORD: "d77c7cb6",
+    DB: "heroku_73ed48707b84af6"
+
+    // mysql://b346791faac424:d77c7cb6@
+    // us-cdbr-east-04.cleardb.com
+    // /heroku_73ed48707b84af6?reconnect=true
   };
 
