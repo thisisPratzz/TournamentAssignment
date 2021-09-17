97e1747d74c9babf71db4086ab74abc3854b512b
Pratik <pratik.k048@gmail.com>
2021-09-17 17:22:35 +0530

    db changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -11,13 +11,23 @@ module.exports = {
 
 
 
-    HOST: "us-cdbr-east-04.cleardb.com",
-    USER: "b346791faac424",
-    PASSWORD: "d77c7cb6",
-    DB: "heroku_73ed48707b84af6"
+    // HOST: "us-cdbr-east-04.cleardb.com",
+    // USER: "b346791faac424",
+    // PASSWORD: "d77c7cb6",
+    // DB: "heroku_73ed48707b84af6"
 
     // mysql://b346791faac424:d77c7cb6@
     // us-cdbr-east-04.cleardb.com
     // /heroku_73ed48707b84af6?reconnect=true
+
+    HOST: "sql6.freemysqlhosting.net",
+    USER: "sql6437937",
+    PASSWORD: "yQ4PjR64td",
+    DB: "sql6437937"
+
+
+
+
+
   };
 
